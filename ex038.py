b6baa49f6e3b3dad4fab493e367383124eda6099
wbcampos9247 <wbcampos@gmail.com>
2024-11-29 11:01:25 -0300

    Exercício final

--- ex038.py
+++ ex038.py
@@ -1,4 +1,9 @@
 n1 = float(input('Primeiro valor: '))
 n2 = float(input('Segundo valor: '))
-print()
-if
+print('COMPARANDO VALORES')
+if n1 > n2:
+    print('O primeiro valor é o maior')
+elif n2 > n1:
+    print('O segundo valor é o maior')
+else:
+    print('Não existe valor maior, os dois são iguais')
