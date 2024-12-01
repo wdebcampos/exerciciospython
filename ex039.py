a88f8eaa87eb4a4c56ec06094306e95de609a3b4
wbcampos9247 <wbcampos@gmail.com>
2024-12-01 19:21:38 -0300

    Exercício final

--- ex039.py
+++ ex039.py
@@ -1,5 +1,7 @@
+from datetime import date
 ano_nascimento = int(input('Ano do nascimento: '))
-ano_atual = 2024
+ano_atual = date.today().year
+print(f'Você nasceu em {ano_nascimento} e possui {ano_atual - ano_nascimento} anos em {ano_atual}')
 if ano_atual - ano_nascimento > 18:
     print('Já passou do tempo de você se alistar')
     print(f'Você deveria ter se alistado em {ano_nascimento + 18}')
@@ -7,4 +9,4 @@ elif ano_atual - ano_nascimento == 18:
     print('Você deve se alistar IMEDIATAMENTE')
 else:
     print('Você ainda vai se alistar')
-    print(f'Falta {ano_atual - ano_nascimento} anos para você se alistar')
+    print(f'Falta {18 - (ano_atual - ano_nascimento)} anos para você se alistar')
