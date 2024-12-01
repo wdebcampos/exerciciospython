702c27bc67268c73f39f22c7bd0a18bf0a9d946b
wbcampos9247 <wbcampos@gmail.com>
2024-12-01 19:28:37 -0300

    Exercício final

--- ex039.py
+++ ex039.py
@@ -1,12 +1,21 @@
 from datetime import date
-ano_nascimento = int(input('Ano do nascimento: '))
-ano_atual = date.today().year
-print(f'Você nasceu em {ano_nascimento} e possui {ano_atual - ano_nascimento} anos em {ano_atual}')
-if ano_atual - ano_nascimento > 18:
-    print('Já passou do tempo de você se alistar')
-    print(f'Você deveria ter se alistado em {ano_nascimento + 18}')
-elif ano_atual - ano_nascimento == 18:
-    print('Você deve se alistar IMEDIATAMENTE')
+sexo = str(input('''
+Digite o sexo:
+ [MAS] - Masculino
+ [FEM] - Feminino
+ ''')).upper()
+if sexo == 'FEM':
+    print('Você não precisa de alistar')
 else:
-    print('Você ainda vai se alistar')
-    print(f'Falta {18 - (ano_atual - ano_nascimento)} anos para você se alistar')
+    print('Você deve se alistar')
+    ano_nascimento = int(input('Ano do nascimento: '))
+    ano_atual = date.today().year
+    print(f'Você nasceu em {ano_nascimento} e possui {ano_atual - ano_nascimento} anos em {ano_atual}')
+    if ano_atual - ano_nascimento > 18:
+        print('Já passou do tempo de você se alistar')
+        print(f'Você deveria ter se alistado em {ano_nascimento + 18}')
+    elif ano_atual - ano_nascimento == 18:
+        print('Você deve se alistar IMEDIATAMENTE')
+    else:
+        print('Você ainda vai se alistar')
+        print(f'Falta {18 - (ano_atual - ano_nascimento)} anos para você se alistar')
