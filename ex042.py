772f15459d9e9f44fd14384f021801c333fbc21d
wbcampos9247 <wbcampos@gmail.com>
2024-12-02 11:40:33 -0300

    Exercício final

--- ex042.py
+++ ex042.py
@@ -1,10 +1,14 @@
 s1 = float(input('Primeiro segmento: '))
 s2 = float(input('Segundo segmento: '))
 s3 = float(input('Terceiro segmento: '))
-print('Com os segmentos informados é possível formar um triângulo do tipo:')
-if s1 == s2 and s2 == s3:
-    print('EQUILÁTERO')
-elif s1 == s2 or s2 == s3 or s1 == s3:
-    print('ISÓCELES')
+print('ANALISADOR DE TRIÂNGULO')
+if s1 + s2 > s3 and s2 + s3 > s1 and s3 + s1 > s2:
+    print('Com os segmentos informados é possível formar um triângulo do tipo: ', end='')
+    if s1 == s2 == s3:
+        print('EQUILÁTERO')
+    elif s1 == s2 or s2 == s3 or s1 == s3:
+        print('ISÓCELES')
+    elif s1 != s2 != s3 != s1:
+        print('ESCALENO')
 else:
-    print('ESCALENO')
+    print('Não é possível formar um triângulo')
