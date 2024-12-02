8ee6d93131d2f063f4dfaf8a1f89cf2683504609
wbcampos9247 <wbcampos@gmail.com>
2024-12-02 10:49:13 -0300

    Exercício final

--- ex040.py
+++ ex040.py
@@ -1,9 +1,10 @@
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
 média = (n1 + n2) / 2
+print(f'Tirante {n1} e {n2} a média do aluno é {média}')
 if média < 5:
-    print(f'sua média é {média:.2f} e você está REPROVADO')
-elif média >= 5 and média <= 6.9:
-    print(f'Sua média é {média:.2f} e você está em RECUPERAÇÃO')
+    print(f'sua média é {média:.1f} e você está REPROVADO')
+elif 6.9 >= média >= 5:
+    print(f'Sua média é {média:.1f} e você está em RECUPERAÇÃO')
 else:
-    print(f'Sua média é {média:.2f} e você está APROVADO')
+    print(f'Sua média é {média:.1f} e você está APROVADO')
