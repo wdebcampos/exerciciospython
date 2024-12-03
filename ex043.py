f9b223b294ebc4c56971e623e843950230e3dcc2
wbcampos9247 <wbcampos@gmail.com>
2024-12-03 07:13:29 -0300

    Exercício final

--- ex043.py
+++ ex043.py
@@ -1,16 +1,16 @@
 print('CALCULADORA DE IMC')
 peso = float(input('Digite o peso: (Kg) '))
 altura = float(input('Digite a altura: (m) '))
-imc = peso / (altura * altura)
-print(f'O seu IMC é {imc:.2f}')
+imc = peso / (altura ** 2)
+print(f'O seu IMC é {imc:.1f}')
 print(f'Sua classificação é: ', end= '')
 if imc < 18.5:
     print('Abaixo do peso')
-elif imc >= 18.5 and imc < 25:
+elif 18.5 >= imc < 25:
     print('Peso ideal')
-elif imc >=25 and imc < 30:
+elif 25 >= imc < 30:
     print('Sobrepesso')
-elif imc >= 30 and imc < 40:
+elif 30 >= imc < 40:
     print('Obesidade')
-elif imc > 40:
+elif imc >= 40:
     print('Obesidade morbida')
