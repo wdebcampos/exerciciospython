7755dd5127a53990ae1b4d938d6081c63916f980
wbcampos9247 <wbcampos@gmail.com>
2025-01-07 10:54:17 -0300

    Exercício final

--- ex068.py
+++ ex068.py
@@ -2,18 +2,16 @@ from random import randint
 print('-=' * 20)
 print(f'{'VAMOS JOGAR PAR OU ÍMPAR':^40}')
 print('-=' * 20)
-computador = randint(1, 10)
 vitoria_jogador = 0
 while True:
     jogador = int(input('Digite um valor: '))
-    opção = str(input('Par ou Ímpar [P/I]? ')).upper().strip()
-    print(f'Você jogou {jogador} e o computador {computador}.')
+    computador = randint(1, 10)
     soma = jogador + computador
-    print(f'Total deu {soma} ', end='')
-    if soma % 2 == 0:
-        print('PAR')
-    else:
-        print('ÍMPAR')
+    opção = ' '
+    while opção not in 'PI':
+        opção = str(input('Par ou Ímpar [P/I]? ')).upper().strip()
+    print(f'Você jogou {jogador} e o computador {computador}. Total deu {soma} ', end='')
+    print(' >>> PAR' if soma % 2 == 0 else ' >>> ÍMPAR')
     if opção == 'P':
         if jogador % 2 == 0:
             print('Você VENCEU')
---
+++ ex069.py
@@ -0,0 +1,25 @@
+print('-' * 30)
+print(f'{'CADASTRE UMA PESSOA':^30}')
+print('-' * 30)
+pessoas_mais_18_anos = homens_cadastrados = mulheres_menos_20_anos = 0
+while True:
+    idade = int(input('Digite a idade: '))
+    if idade > 18:
+        pessoas_mais_18_anos += 1
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Digite o sexo: ')).upper().strip()[0]
+    if sexo == 'F' and idade < 20:
+        mulheres_menos_20_anos += 1
+    if sexo == 'M':
+        homens_cadastrados += 1
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? ')).upper().strip()[0]
+    if continuar == 'N':
+        break
+    print('-' * 30)
+print('-' * 35)
+print(f'{pessoas_mais_18_anos} pessoas tem mais de 18 anos')
+print(f'Foram cadastrados {homens_cadastrados} homens')
+print(f'{mulheres_menos_20_anos} mulheres tem menos de 20 anos')
