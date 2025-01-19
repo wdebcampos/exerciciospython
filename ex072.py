c2264b8e4fb164a9e8b6c57227a272b3c8aa4f4c
wbcampos9247 <wbcampos@gmail.com>
2025-01-19 07:34:18 -0300

    Exercício final

---
+++ ex072.py
@@ -0,0 +1,12 @@
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',  'seis',
+           'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+           'treze', 'quatorze', 'quinze', 'dezeseis',
+           'dezesete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    nún = int(input('Digite um número entre 0 e 20: '))
+    if nún < 0 or nún > 20:
+        print(f'tente novamente.', end='')
+    else:
+        print(extenso[nún])
+        break
+
