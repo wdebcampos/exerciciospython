f3050c4ca81cd24014eafa11057caad93e4b9b65
wbcampos9247 <wbcampos@gmail.com>
2025-01-22 14:31:31 -0300

    Exercício final

--- ex075.py
+++ ex075.py
@@ -1,16 +1,14 @@
-num = (int(input('Digite um número ')),
-       int(input('Digite um número ')),
-       int(input('Digite um número ')),
-       int(input('Digite um número ')))
+num = (int(input('Digite um número: ')),
+       int(input('Digite um número: ')),
+       int(input('Digite um número: ')),
+       int(input('Digite um número: ')))
 print(f'Você digitou os valores:', ','.join(map(str, num)))
 print(f'O número 9 apareceu {num.count(9)}')
 if 3 in num:
-    print(f'O valor 3 aparece na {num.count(3)}ª posição')
+    print(f'O valor 3 aparece na {num.index(3)+1}ª posição')
 else:
     print('O número 3 não foi digitado')
 print(f'Os números pares digitados foram: ', end='')
-cont = 0
 for n in num:
     if n % 2 == 0:
-        cont += 1
         print(n, end=' ')
