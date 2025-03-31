7a008d7a0fbb71c465517a8dd96a929580de2ed3
wbcampos9247 <wbcampos@gmail.com>
2025-03-31 10:17:17 -0300

    final

---
+++ ex078.py
@@ -0,0 +1,22 @@
+listanum = []
+maior = menor = 0
+for c in range(0,5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        maior = menor = listanum[c]
+    else:
+        if listanum[c] > maior:
+            maior = listanum[c]
+        if listanum[c] < menor:
+            menor = listanum[c]
+print('=-' * 30)
+print(f'Você digitou os valores: {listanum}')
+print(f'O maior valor digitado foi {maior}, nas posições ', end='')
+for indice, valor in enumerate(listanum):
+    if valor == maior:
+        print(f'{indice}...', end='')
+print()
+print(f'O menor valor digitado foi {menor}, nas posições ', end='')
+for indice, valor in enumerate(listanum):
+    if valor == menor:
+        print(f'{indice}...', end='')
